bbf59bc29ad08b6707d6b57d8567321add99b3ab
Pio dos Santos <piodanilod@gmail.com>
2018-03-18 23:37:28 -0300

    implementado como busqueda

--- UT1 - Problema y estrategias de Busqueda/CardGame.py
+++ UT1 - Problema y estrategias de Busqueda/CardGame.py
@@ -1,5 +1,6 @@
 import random
 import math
+import copy
 def DeckCards():
 	cards=[]
 	#"♠♥♦♣"
@@ -7,9 +8,12 @@ def DeckCards():
 	for i in "♠♥♦♣":
 		k = 0
 		for j in ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]:
-			cards+= [(j+" "+i,k)]
+			cards+= [(j+" "+i+" ",k)]
 			k+=1
-	return cards
+	cards2=[]
+	for i in range(52):
+		cards2+=[cards.pop(random.randint(0,len(cards)-1))]
+	return cards2
 
 # lista montones [1,3,4,5]
 def RandomDeckCards(overlapping_list):
@@ -21,48 +25,90 @@ def RandomDeckCards(overlapping_list):
 	result+=[deck]
 	return result
 
-# Sucesores
-def sucesor(deck):
-	cantidad_mazos= len(deck) -2
-	tope_fundacion= deck[-2][-1]
-	resultado=[]
+# successores
+def successor(deck):
+	number_decks= len(deck) -2
+	foundation= deck[-2][-1]
+	result=[]
 	if len(deck[-1]) != 0:
-		resultado+=[-1]
-	for i in range(cantidad_mazos):
+		result+=[-1]
+	for i in range(number_decks):
 		if deck[i] != [] :
-			if deck[i][-1][1]==((tope_fundacion[1] + 1)%13):
-				resultado+=[i]
-			elif deck[i][-1][1]==((tope_fundacion[1] - 1)%13):
-				resultado+=[i]
-	return resultado
+			if deck[i][-1][1]==((foundation[1] + 1)%13):
+				result+=[i]
+			elif deck[i][-1][1]==((foundation[1] - 1)%13):
+				result+=[i]
+	return result
 
-def play(game, camino):
-	if len(game[-2])==52:
-		return (True,camino)
-	sucesores = sucesor(game)
-	for s in sucesores:
+def search1(problem):
+	for _ in range(25):
+		deck = RandomDeckCards(problem)
+		if goalTest1(problem,copy.deepcopy(deck)):
+			return deck
+	raise Exception("Search 1: Fail")
+
+def goalTest1(problem,solution):
+	if len(problem)!=(len(solution)-2):
+		print("entro aca")
+		return False
+	p=0
+	for i in problem:
+		if len(solution[p])!=i:
+			print("entro a monton distinto.")
+			return False
+		p+=1
+	try:
+		a=search2(solution)
+	except Exception as e:
+		a=False
+	finally:
+		return a
+
+
+def search2(problem):
+	result = search2_recursive(problem,[])
+	if result[0]:
+		return result[1]
+	raise Exception("Search 2: Fail")
+
+def search2_recursive(game, sequence):
+	if goalTest2(game,sequence):
+		return (True,sequence)
+	successores = successor(game)
+	for s in successores:
 		#print(len(game[-2]))
 		game[-2]+=[game[s].pop()]
-		(resultado,new_path)= play(game,camino+[s])
-		if resultado:
+		(result,new_path)= search2_recursive(game,sequence+[s])
+		if result:
 			return (True,new_path)
 		game[s]+=[game[-2].pop()]
-	return (False,camino)
+	return (False,sequence)
+
+def goalTest2(problem,solution):
+	return len(problem[-2])==52
 
 def test():
-	resultado = RandomDeckCards([7,7,7,7,7])
+	result = RandomDeckCards([7,7,7,7,7])
 	print("tope mazos en mesa")
-	for i in range( len(resultado)-2):
-		print(resultado[i][-1][0])
+	for i in range( len(result)-2):
+		print(result[i][-1][0])
 	print("Fundacion")
-	print(resultado[-2][-1][0])
+	print(result[-2][-1][0])
 	print("Tope Mazo ")
-	print(resultado[-1][-1][0])
+	print(result[-1][-1][0])
 	print("Combinaciones posibles")
-	print(sucesor(resultado))
+	print(successor(result))
 def test1():
-	juego = RandomDeckCards([5,4,4,5,1,1,2])
-	print(juego)
-	juego = play(juego,[])
-	print(juego[0],len(juego[1]),juego[1])
-test1()
+	juego = RandomDeckCards([48,1])
+	for i in range(4):
+		print("Mazo "+str(i)+":"+str([j[0] for j in juego[i]]))
+	print("Foundation: "+str(juego[-2]))
+	print("Mazo: "+str([j[0] for j in juego[-1]]))
+	juego = search2(juego)
+	print(len(juego),juego)
+
+
+def test2():
+	a = search1([49,1])
+	print(a)
+test2()
