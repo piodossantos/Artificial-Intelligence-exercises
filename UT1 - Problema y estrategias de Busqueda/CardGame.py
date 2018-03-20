9205d391d070121ae8ab1bd21714c3f75ef8c18e
Pio dos Santos <piodanilod@gmail.com>
2018-03-20 16:04:19 -0300

    ut1

--- UT1 - Problema y estrategias de Busqueda/CardGame.py
+++ UT1 - Problema y estrategias de Busqueda/CardGame.py
@@ -30,14 +30,14 @@ def successor(deck):
 	number_decks= len(deck) -2
 	foundation= deck[-2][-1]
 	result=[]
-	if len(deck[-1]) != 0:
-		result+=[-1]
 	for i in range(number_decks):
 		if deck[i] != [] :
 			if deck[i][-1][1]==((foundation[1] + 1)%13):
 				result+=[i]
 			elif deck[i][-1][1]==((foundation[1] - 1)%13):
 				result+=[i]
+	if len(deck[-1]) != 0:
+		result+=[-1]
 	return result
 
 def search1(problem):
@@ -64,19 +64,19 @@ def goalTest1(problem,solution):
 	finally:
 		return a
 
-
 def search2(problem):
 	result = search2_recursive(problem,[])
 	if result[0]:
 		return result[1]
 	raise Exception("Search 2: Fail")
 
-def search2_recursive(game, sequence):
+def search2_recursive(game,sequence):
+	##El test objetivo podria plantearse como problema, y secuencia, pero no es eficiente cuando se recorren millones de nodos
+	##una simplificacion es manejar el estado actual del juego, recalcularlo cada vez que se va a evaluar no es eficiente.
 	if goalTest2(game,sequence):
 		return (True,sequence)
 	successores = successor(game)
 	for s in successores:
-		#print(len(game[-2]))
 		game[-2]+=[game[s].pop()]
 		(result,new_path)= search2_recursive(game,sequence+[s])
 		if result:
@@ -87,28 +87,37 @@ def search2_recursive(game, sequence):
 def goalTest2(problem,solution):
 	return len(problem[-2])==52
 
-def test():
-	result = RandomDeckCards([7,7,7,7,7])
-	print("tope mazos en mesa")
-	for i in range( len(result)-2):
-		print(result[i][-1][0])
-	print("Fundacion")
-	print(result[-2][-1][0])
-	print("Tope Mazo ")
-	print(result[-1][-1][0])
-	print("Combinaciones posibles")
-	print(successor(result))
-def test1():
-	juego = RandomDeckCards([48,1])
-	for i in range(4):
-		print("Mazo "+str(i)+":"+str([j[0] for j in juego[i]]))
-	print("Foundation: "+str(juego[-2]))
-	print("Mazo: "+str([j[0] for j in juego[-1]]))
-	juego = search2(juego)
-	print(len(juego),juego)
+
+def printGame(game):
+	for i in range(len(game)-2):
+		print("Mazo "+str(i)+":"+str([j[0] for j in game[i]]))
+	print("Fundacion: "+str(game[-2][0][0]))
+	print("Mazo sin descubrir: "+str([j[0] for j in game[-1]]))
 
 
-def test2():
-	a = search1([49,1])
-	print(a)
-test2()
+def testRandomDeck():
+	print("\nEjemplo de movientos disponibles en mazo aleatorio\n")
+	result = RandomDeckCards([7,7,7,7,7,7])
+	printGame(result)
+	print("Sucsores: "+ str(successor(result)))
+
+def testSearch1():
+	print("\nEjemplo de Busqueda de solucion a un juego\n")
+	overlapping_list = [4,4,4,4]
+	juego = RandomDeckCards(overlapping_list)
+	printGame(juego)
+	try:
+		juego = search2(juego)
+		print("Camino: " + str(juego))
+	except Exception as e:
+		print("Este juego no tiene Solucion")
+
+
+def testSearch2():
+	print("\nEjemplo de Busqueda de juego que pueda resolverse\n")
+	a = search1([4,4,4,4])
+	printGame(a)
+
+testSearch1()
+testSearch2()
+testRandomDeck()
